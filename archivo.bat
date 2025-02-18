abd1d833bfe8b06a00fb2c2bb0a80951f9ac6334
Saul Ruiz <87398872+saulsmith21@users.noreply.github.com>
2025-02-17 22:33:22 -0600

    Update archivo.bat

--- archivo.bat
+++ archivo.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo Extracion de archivos ZIP.
+echo Beta 0.1. | GNU General Public License v3.0
+echo Se van a extraer todos los archivos contenidos en los archivos ZIP en el directorio actual.
+pause
+
 :: Obtener el directorio actual
 set "dir=%CD%"
 
 :: Recorrer los archivos ZIP en el directorio
-
 for %%F in ("%dir%\*.zip") do (
     echo Extrayendo %%F...
     powershell -command "Expand-Archive -LiteralPath '%%F' -DestinationPath '%dir%' -Force"
